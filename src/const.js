fd5f5d189225215b3ffc3081034d99ae019b7c70
BlackBox12345 <kimkir38@gmail.com>
2025-05-05 16:53:00 +0500

    module2-task1

---
+++ src/const.js
@@ -0,0 +1,51 @@
+const TYPE = [
+    {
+        id: 1,
+        name: "Taxi",
+        img: "taxi.png",
+    },
+    {
+        id: 2,
+        name: "Bus",
+        img: "bus.png",
+    },
+    {
+        id: 3,
+        name: "Train",
+        img: "train.png",
+    },
+    {
+        id: 4,
+        name: "Ship",
+        img: "ship.png",
+    },
+    {
+        id: 5,
+        name: "Drive",
+        img: "drive.png",
+    },
+    {
+        id: 6,
+        name: "Flight",
+        img: "flight.png",
+    },
+    {
+        id: 7,
+        name: "Check-In",
+        img: "check-in.png",
+    },
+    {
+        id: 8,
+        name: "Sightseeing",
+        img: "sightseeing.png",
+    },
+    {
+        id: 9,
+        name: "Restaurant",
+        img: "restaurant.png",
+    }
+];
+
+const EVENT_COUNT = 5;
+
+export { TYPE, EVENT_COUNT };
--- src/view/view-event.js
+++ src/view/view-event.js
@@ -1,32 +1,46 @@
 import {createElement} from '../render.js';
+import {
+  humanizeEventDueDate,
+  humanizeEventTime,
+  formatDuration,
+} from "../utils.js";
 
-function createViewEvent() {
+function createViewEvent(point) {
+  const {
+    type,
+    destination,
+    dateFrom,
+    dateTo,
+    basePrice,
+    offersHtml,
+    favorite,
+  } = point;
+ 
+  const date = humanizeEventDueDate(dateFrom);
+  const timeStart = humanizeEventTime(dateFrom);
+  const timeEnd = humanizeEventTime(dateTo);
   return `
     <li class="trip-events__item">
               <div class="event">
-                <time class="event__date" datetime="2019-03-18">MAR 18</time>
+                <time class="event__date" datetime="2019-03-18">${date}</time>
                 <div class="event__type">
-                  <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+                  <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
                 </div>
-                <h3 class="event__title">Taxi Amsterdam</h3>
+                <h3 class="event__title">${type} ${destination.name}</h3>
                 <div class="event__schedule">
                   <p class="event__time">
-                    <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+                    <time class="event__start-time" datetime="2019-03-18T10:30">${timeStart}</time>
                     &mdash;
-                    <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+                    <time class="event__end-time" datetime="2019-03-18T11:00">${timeEnd}</time>
                   </p>
-                  <p class="event__duration">30M</p>
+                  <p class="event__duration">${formatDuration(dateFrom, dateTo)}</p>
                 </div>
                 <p class="event__price">
-                  &euro;&nbsp;<span class="event__price-value">20</span>
+                  &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
                 </p>
                 <h4 class="visually-hidden">Offers:</h4>
                 <ul class="event__selected-offers">
-                  <li class="event__offer">
-                    <span class="event__offer-title">Order Uber</span>
-                    &plus;&euro;&nbsp;
-                    <span class="event__offer-price">20</span>
-                  </li>
+                ${offersHtml}
                 </ul>
                 <button class="event__favorite-btn event__favorite-btn--active" type="button">
                   <span class="visually-hidden">Add to favorite</span>
@@ -43,8 +57,12 @@ function createViewEvent() {
 }
 
 export default class ViewEvent {
+  constructor({ point }) {
+    this.point = point;
+  }
+  
   getTemplate() {
-    return createViewEvent();
+    return createViewEvent(this.point);
   }
 
   getElement() {
